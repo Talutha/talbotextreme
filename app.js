a6ec19b7d552cd0c3811cc914f6b6ec639e7f8ed
Talutha <talutha@gmail.com>
2017-08-03 03:48:04 -0500

    initial implementation of massivejs

--- app.js
+++ app.js
@@ -1,34 +1,61 @@
-var tmi = require('tmi.js');
+const tmi = require('tmi.js');
+const massive = require('massive');
 
-var options = {
-  options: {
-    debug: true
-  },
-  connection: {
-    reconnect: true
-  },
-  identity: {
-    username: "TalBotExtreme",
-    password: "changeme"
-  },
-  channels: ['#talbotextreme']
+// db connection info(psql)
+const connectionInfo = {
+  host: 'localhost',
+  port: 5432,
+  database: 'test_commands_db',
+  user: 'test_commands_db',
+  password: 'testing',
+  ssl: false,
+  poolSize: 10
 };
 
-var client = new tmi.client(options);
+// connect to db
+massive(connectionInfo).then(db => {
 
-// Connect the client to the server..
-client.connect();
+  db.commands.find({
+    command_name: 'testing'
+  }).then(result => {
+    console.log(result[0].command_output);
+  });
 
-client.on('connected', function (address, port) {
-  client.say('#talbotextreme', 'Hello world, I am back...')
-});
 
-client.on('chat', function (channel, userstate, message, self) {
-  // Don't listen to my own messages...
-  if (self) return;
 
-  if (message === '!howdy') {
-    console.log(userstate);
-    client.say(channel, 'Howdy there ' + userstate['display-name'] + '!');
+  var options = {
+    options: {
+      debug: true
+    },
+    connection: {
+      reconnect: true
+    },
+    identity: {
+      username: "TalBotExtreme",
+      password: "changeme"
+    },
+    channels: ['#talbotextreme']
   };
-})
+
+  var client = new tmi.client(options);
+
+  // Connect the client to the server..
+  //client.connect();
+
+  /*
+  client.on('connected', function (address, port) {
+    client.say('#talbotextreme', 'Hello world, I am back...')
+  });
+  */
+
+  client.on('chat', function (channel, userstate, message, self) {
+    // Don't listen to my own messages...
+    if (self) return;
+
+    if (message === '!howdy') {
+      console.log(userstate);
+      client.say(channel, 'Howdy there ' + userstate['display-name'] + '!');
+    };
+  })
+
+});
