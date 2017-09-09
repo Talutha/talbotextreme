f401df275aa7c2c1c4bcf146f369488144691a0e
Chris Crowder <chris.crowder.95@gmail.com>
2017-09-09 14:51:50 -0400

    add column announce_sent

--- db/tableCreation/createDiscordAnnouncersTable.sql
+++ db/tableCreation/createDiscordAnnouncersTable.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS discord_announcers (
     discord_guild_id BIGINT,
     discord_channel_id BIGINT,
     twitch_channel_name VARCHAR,
+    announce_sent BOOLEAN,
     last_announce TIMESTAMP WITHOUT TIME ZONE
 );
